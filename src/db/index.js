b73eeea4f8b1e8aa439e8f89197976c162c8341c
yashsharma4321 <ys0979727@gmail.com>
2024-04-18 21:48:10 +0530

    add and video model

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+
+const connectDb = async () => {
+    try {
+        const connnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected!! DB Host: ${connnectionInstance.connection.host}`);
+    }
+    catch(error) {
+        console.log("MONGODB connection error:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDb;
